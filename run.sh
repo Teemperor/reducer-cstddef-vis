9bdc2c88891fba5eff5768903e773b72a04a9646
Raphael Isemann <teemperor@gmail.com>
2020-02-20 08:57:04 +0100

    reduced

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 rm *.tmp
 rm -rf pcms
 echo "NORMAL"
-$1 -H -Icxx -nostdinc++  -isysroot "/Users/teemperor/test/reduce/sdk" test.cpp -c
+$1 -H -Icxx -nostdinc++ test.cpp -c
 echo "VIS MODULES"
-$1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -isysroot "/Users/teemperor/test/reduce/sdk" -fmodules -Xclang -fmodules-local-submodule-visibility test.cpp -c
+$1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -fmodules -Xclang -fmodules-local-submodule-visibility test.cpp -c
 echo "MODULES"
-$1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -isysroot "/Users/teemperor/test/reduce/sdk" -fmodules -fcxx-modules test.cpp -c
+$1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -fmodules -fcxx-modules test.cpp -c
 
