f33ccbbc90bb33de3db104a1f38436451596f512
Raphael Isemann <teemperor@gmail.com>
2020-02-20 10:53:52 +0100

    renamed cxx to inc

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
-echo "NORMAL (with -H)"
-$1 -H -Icxx -nostdinc++ test.cpp -c
-echo "VIS MODULES"
-$1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -fmodules -Xclang -fmodules-local-submodule-visibility test.cpp -c
-echo "MODULES (this will fail)"
-$1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -fmodules -fcxx-modules test.cpp -c
+#!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+clang=$1
+
+cd "$DIR"
+rm -rf pcms
+echo "NORMAL (with -H)"
+$clang -H -Iinc -nostdinc++ test.cpp -c
+echo "VIS MODULES"
+$clang -Iinc -nostdinc++ -fmodules-cache-path=pcms -fmodules -Xclang -fmodules-local-submodule-visibility test.cpp -c
+rm -rf pcms
+echo "MODULES (this will fail)"
+$clang -Iinc -nostdinc++ -fmodules-cache-path=pcms -fmodules -fcxx-modules test.cpp -c
+rm -rf pcms
