0db01803f719e9856b93346fc357cde04745e298
Raphael Isemann <teemperor@gmail.com>
2020-02-20 08:57:52 +0100

    comment run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
-rm *.tmp
-rm -rf pcms
-echo "NORMAL"
+echo "NORMAL (with -H)"
 $1 -H -Icxx -nostdinc++ test.cpp -c
 echo "VIS MODULES"
 $1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -fmodules -Xclang -fmodules-local-submodule-visibility test.cpp -c
-echo "MODULES"
+echo "MODULES (this will fail)"
 $1 -Icxx -nostdinc++ -fmodules-cache-path=pcms -fmodules -fcxx-modules test.cpp -c
 
